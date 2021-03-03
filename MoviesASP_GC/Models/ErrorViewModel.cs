602f6249be86deb698f2ba4a8e13ba238a399b32
Josh <josh_2021@yahoo.com>
2021-03-03 13:21:01 -0500

    initial commit

---
+++ MoviesASP_GC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoviesASP_GC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
